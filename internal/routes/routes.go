ff4d55878ffed72c0eba88a6c887064f89cc00ae
prateekpurohit13 <prateekpurohit171@gmail.com>
2025-03-24 21:50:56 +0530

    Added code files for extracting data from .har files and made some minor changes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/RavenSec10/Raven_Backend/internal/handlers"
 )
 
 // SetupRoutes initializes all API routes
-func SetupRoutes(router *gin.Engine) {
+func SetupRoutes(router *gin.Engine, harHandler *handlers.HARHandler) {
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Welcome to RAVEN API"})
 	})
-	router.POST("/upload-har", UploadHAR) // This will call the function from upload.go
+	router.POST("/upload-har", harHandler.UploadHAR) // This will call the function from upload.go
 }
--- main.go
+++ main.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"log"
-	"net/http"
+	//"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/RavenSec10/Raven_Backend/internal/routes"
+	"github.com/RavenSec10/Raven_Backend/internal/services"
+	"github.com/RavenSec10/Raven_Backend/internal/handlers"
+
+
 )
 
 func main() {
+
+	harService := services.NewHARService()
+	harHandler := handlers.NewHARHandler(harService)
 	router := gin.Default()
 
+	routes.SetupRoutes(router, harHandler)
+
 	// Add a root route for testing
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Welcome to RAVEN API"})
-	})
+	//router.GET("/", func(c *gin.Context) {
+	//	c.JSON(http.StatusOK, gin.H{"message": "Welcome to RAVEN API"})
+	//})
 
 	// Register the upload route
-	router.POST("/upload", routes.UploadHAR)
+	//router.POST("/upload", harHandler.UploadHAR)
 
 	log.Println("Server running on :8080")
 	router.Run(":8080")
